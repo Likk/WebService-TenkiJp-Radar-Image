1dc2e126f74db3d4fc66c94a1bd77f8d20bfe699
likk <perl@li.que.jp>
2015-05-25 19:08:54 +0900

    レーダー予報gifアニメーション取得サンプル

---
+++ example/get_image_japan_forecast_animation.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Temp qw/tempfile/;
+use GD;
+use WebService::TenkiJp::Radar::Image;
+
+my $gifdata;
+my @forecast = qw/60 120 180 240 300 360/;
+for my $index (0..$#forecast){
+    my $image = WebService::TenkiJp::Radar::Image->new()->get_image(forecast => $forecast[$index]);
+
+    #一旦ファイルに保存してから GD で画像を読み込む
+    my ($fh, $path ) = tempfile();
+    binmode $fh;
+    print $fh $image;
+    close $fh;
+    my $frame = GD::Image->new($path);
+    unlink $path; #読み込んだら元ファイルは不要
+
+    if($index == 0){
+        $gifdata = $frame->gifanimbegin(1,0);
+    }
+
+    #gif animation としてデータ連結
+    $gifdata .= $frame->gifanimadd(1,0,0,100);
+
+    if($index == $#forecast){
+        $gifdata .= $frame->gifanimend;
+    }
+
+}
+
+open my $fh, '>', './image_ja_forecast_animation.gif';
+binmode $fh;
+print $fh $gifdata;
+close $fh;
